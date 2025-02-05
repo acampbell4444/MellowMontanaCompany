1359a33bbdbe002bcd55f6dba144daf7af5a4bb4
Alan Campbell <alancampbell@Alans-MBP.attlocal.net>
2025-02-04 21:48:23 -0800

    mellow in business

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-import App from './App.tsx'
-
+import App from "./App.tsx";
 
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
